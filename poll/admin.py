f86b19202a2ef73bdd6891e10b7bb7591b03e6b7
18627659723@163.com <150>
2017-10-27 11:48:41 +0800

    20171027使用layui进行操作

---
+++ poll/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date'] #用来过滤
+    search_fields = ['question_text']#用来搜索
+    list_per_page = 50 #控制分页
+
+admin.site.register(Question, QuestionAdmin)
---
+++ poll/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+import datetime
+from django.utils import timezone
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):  # __unicode__ on Python 2
+        return self.question_text
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def save(self, *args, **kwargs):
+        print("ddd")
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):  # __unicode__ on Python 2
+        return self.choice_text
